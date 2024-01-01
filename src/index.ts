c947e6f91193eba53e038e48e8a1d89090434839
joshihemal <hemaljoshi738@gmail.com>
2024-01-01 06:11:11 +0530

    initialize project

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+let PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
+});
